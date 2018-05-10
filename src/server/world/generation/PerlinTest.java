34a78a03da01144deb6d0651c114ef7de791a9ce
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2018-05-10 17:42:54 +0100

    Playing around with adding world boundaries based on position from the world centre.

--- src/server/world/generation/PerlinTest.java
+++ src/server/world/generation/PerlinTest.java
@@ -22,20 +22,31 @@ public class PerlinTest {
 		File outputfile   = new File(System.currentTimeMillis() + "_map.png");
 		BufferedImage img = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
 		
+		// Our world size must be an even number.
+		if ((worldSize % 2) != 0) {
+			throw new RuntimeException("World size must be an even number!");
+		}
+		
+		// Calculate the distance between the world centre and the edge.
+		int centreToEdge = worldSize / 2;
+		
 		// Randomly generate a z position for our map based on our seed.
 		double z = rng.nextDouble() * (rng.nextInt(1000));
 		
 		// Generate each tile static tile and draw it to our world image.
-		for (int x = 0; x < size; x++) {
-			for (int y = 0; y < size; y++) {
+		for (int x = -centreToEdge; x < centreToEdge; x++) {
+			for (int y = -centreToEdge; y < centreToEdge; y++) {
 				// Generate terrain noise for the current world position.
-				int terrainNoise = (int)(generateNoise(0.02f, worldSize, x, y, z) * 10);
+				int terrainNoise = (int)(generateNoise(0.02f, x, y, z) * 10);
 				// Generate biome noise for the current world position.
-				int biomeNoise = (int)(generateNoise(0.025f, worldSize, x, y, z + 100) * 10);
+				int biomeNoise = (int)(generateNoise(0.025f, x, y, z + 100) * 10);
+				// Generate boundary noise for the current world position.
+				// This value will raise the edges of the world to produce the cold lands.
+				int boundaryNoise = (int)(generateBoundaryNoise(0.025f, centreToEdge, x, y, z) * 5);
 				// Convert this piece of noise to a tile type.
-				TileType tileType = convertNoiseToTileType(terrainNoise, biomeNoise);
+				TileType tileType = convertNoiseToTileType(terrainNoise - boundaryNoise, biomeNoise);
 				// Draw the current tile to our world image.
-				drawTileToWorldImage(img, x, y, tileType);
+				drawTileToWorldImage(img, centreToEdge, x, y, tileType);
 			}
 		}
 		
@@ -49,11 +60,12 @@ public class PerlinTest {
 	/**
 	 * Draw a tile as a pixel to a world image.
 	 * @param image The world image.
+	 * @param centreToEdge The distance between the world centre and the edge.
 	 * @param x The x world position.
 	 * @param y The y world position.
 	 * @param tileType The tile type at the position.
 	 */
-	private static void drawTileToWorldImage(BufferedImage image, int x, int y, TileType tileType) {
+	private static void drawTileToWorldImage(BufferedImage image, int centreToEdge, int x, int y, TileType tileType) {
 		int r = 0;
 		int g = 0;
 		int b = 0;
@@ -115,7 +127,7 @@ public class PerlinTest {
 		// Create the colour.
 		int colour = (r << 16) | (g << 8) | b;
 		// Set the pixel colour at the x/y position.
-		image.setRGB(x, y, colour);
+		image.setRGB(x + centreToEdge, y + centreToEdge, colour);
 	}
 	
 	/**
@@ -192,6 +204,22 @@ public class PerlinTest {
 		// Return the tile type.
 		return type;
 	}
+	
+	/**
+	 * Generate boundary noise for a world position.
+	 * @param frequency The frequency.
+	 * @param centreToEdge The distance between the world centre and the edge.
+	 * @param x The x value.
+	 * @param y The y value.
+	 * @param z The z value.
+	 * @return The noise value for the position.
+	 */
+	private static double generateBoundaryNoise(float frequency, int centreToEdge, int x, int y, double z) {
+		// Generate a Manhattan distance value.
+		double distance = Math.abs(x) + Math.abs(y);
+		// Generate a piece of noise for this x/y position.
+		return (ImprovedNoise.noise(x * frequency, y * frequency, z) + 0.8) * (1 - Math.pow(distance / centreToEdge, 3));
+	}
 
 	/**
 	 * Generate noise for a world position.
@@ -201,7 +229,7 @@ public class PerlinTest {
 	 * @param z The z value.
 	 * @return The noise value for the position.
 	 */
-	private static double generateNoise(float frequency, int worldSize, int x, int y, double z) {
+	private static double generateNoise(float frequency, int x, int y, double z) {
 		float nx = x * frequency;
 		float ny = y * frequency;
 		// Generate a piece of noise for this x/y position.
