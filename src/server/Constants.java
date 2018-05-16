98bff16880b11ba0b072297e26e3e72e815ed880
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2018-05-16 13:10:15 +0100

    Added ServerClock

--- src/server/Constants.java
+++ src/server/Constants.java
@@ -5,9 +5,12 @@ package server;
  */
 public class Constants 
 {
-	/** The size of the world (chunks). */
+	/** The size of the world (tiles). */
 	public static int WORLD_SIZE = 1024;
 	
 	/** The size of a chunk (tiles). */
 	public static int WORLD_CHUNK_SIZE = 32;
+	
+	/** The server clock rate (ms). */
+	public static long SERVER_CLOCK_RATE = 125l;
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,10 +1,26 @@
 package server;
 
+/**
+ * The game server.
+ */
 public class Server 
 {
-
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello World!");
+	/**
+	 * Program entry point.
+	 * @param args The command line arguments.
+	 */
+	public static void main(String[] args) {
+		// Create a server clock and pass it a new instance of Server.
+		ServerClock serverClock = new ServerClock(new Server());
+		// Start the server clock. 
+		serverClock.start();
+	}
+	
+	/**
+	 * The server loop.
+	 * This is called at a consistent rate by the server clock.
+	 */
+	public void loop() {
+		System.out.println("LOOP");
 	}
 }
