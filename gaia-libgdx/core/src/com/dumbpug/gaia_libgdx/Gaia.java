623f67b219024f27054e196f95015275bf5fae74
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2018-08-15 13:35:54 +0100

    Creating VerverProxy instance within running LibGdx app.

--- gaia-libgdx/core/src/com/dumbpug/gaia_libgdx/Gaia.java
+++ gaia-libgdx/core/src/com/dumbpug/gaia_libgdx/Gaia.java
@@ -1,10 +1,13 @@
 package com.dumbpug.gaia_libgdx;
 
+import java.io.IOException;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import gaia.client.networking.ServerJoinRequestRejectedException;
+import gaia.client.networking.ServerProxy;
 
 public class Gaia extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -14,11 +17,19 @@ public class Gaia extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+		try {
+			ServerProxy server = ServerProxy.create("localhost", 23445, "nikolas howard");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ServerJoinRequestRejectedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
