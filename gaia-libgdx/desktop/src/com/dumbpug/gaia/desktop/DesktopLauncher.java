963451cf0e8482fd26751f8f1eef0ba9b4ecb09c
Nikolas Howard <nikolas.howard@LighthouseSystems.com>
2018-08-15 13:13:06 +0100

    Added LibGdx project alonside server-client project

---
+++ gaia-libgdx/desktop/src/com/dumbpug/gaia/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dumbpug.gaia.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dumbpug.gaia_libgdx.Gaia;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Gaia(), config);
+	}
+}
